5d30288163fe60cfb3e10ab5dec028a9ba410ea3
Mohamed Amine Belabbes <m.amine.belabbes@gmail.com>
2020-11-21 15:33:31 +0000

    Set up quick reply

--- back-end/bot_behaviour.py
+++ back-end/bot_behaviour.py
@@ -1,14 +1,49 @@
 import config
 
-def createWelcomeMessage(new_welcome_message):
+
+def createWelcomeMessage(oauth, new_welcome_message):
     response = oauth.post("https://api.twitter.com/1.1/direct_messages/welcome_messages/new.json",
-              json={
-                      "welcome_message" : {
-                        "name": "codechella_welcome-message 01",
-                        "message_data": {
-                          "text": "Welcome this is the dream team!"
+                          json={
+                              "welcome_message": {
+                                  "name": "codechella_welcome-message 01",
+                                  "message_data": {
+                                      "text": f"{new_welcome_message}",
+                                      "quick_reply": {
+                                          "type": "options",
+                                          "options": [
+                                              {
+                                                  "label": "Community",
+                                                  "description": "Connect with your community",
+                                                  "metadata": "external_id_1"
+                                              },
+                                              {
+                                                  "label": "Topics",
+                                                  "description": "Match with a fellow bird to chat",
+                                                  "metadata": "external_id_2"
+                                              }
+                                          ]
+                                      }
+                                  }
+                              }
                           }
-                        }
-                      }
-                    )
+                          )
     return response
+
+
+def setWelcomeMessage(oauth, welcome_message_id):
+    response = oauth.post("https://api.twitter.com/1.1/direct_messages/welcome_messages/rules/new.json",
+                          json={
+                              "welcome_message_rule": {
+                                  "welcome_message_id": f"{welcome_message_id}"
+                              }
+                          })
+    return response
+
+
+def deleteWelcomeMessageRule(oauth, rule_id):
+    return oauth.delete(
+        f"https://api.twitter.com/1.1/direct_messages/welcome_messages/rules/destroy.json?id={rule_id}")
+
+
+def getWelcomeMessageRules(oauth):
+    return oauth.get("https://api.twitter.com/1.1/direct_messages/welcome_messages/rules/list.json")
