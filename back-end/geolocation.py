25fc01416b614de53efaa496b1fcefb62873d981
Teleayo Oyekunle <randomlyinspirational@gmail.com>
2020-11-21 20:22:30 +0100

    Added function to find distances between 2 points on the map

--- back-end/geolocation.py
+++ back-end/geolocation.py
@@ -1,9 +1,32 @@
 import requests
 import os
 from dotenv import load_dotenv
+from geopy import distance
 load_dotenv()
 
 GOOGLE_API_KEY = os.environ.get('GOOGLE_GEOLOCATION_KEY', "")
+
+def distanceBetweenLocations(locA, locB, inMiles):
+    """Returns the distance between two locations
+
+    Parameters
+    ----------
+    locA: tuple
+        Has two attributes: (lat, lng)
+    locB: tuple
+        Has two attributes: (lat, lng)
+
+    metric: str
+        Used either 'mi' or 'km' to get your result in that format.
+    Returns
+    -------
+    double
+        the distance between the two locations
+    """
+    if inMiles:
+        return distance.distance(locA, locB).miles
+    return distance.distance(locA, locB).km
+
 
 def getLocationInfo(location_str):
     """Returns Location information of a given address
@@ -47,4 +70,16 @@ if __name__=="__main__":
     """
     To test module functionality
     """
-    print(getLocationInfo("gbagada phase 2lagos"))
+    # print(getLocationInfo("gbagada phase 2lagos"))
+    addr1 = getLocationInfo("panorama hotel, Victoria Island")
+    addr2 = getLocationInfo("gbagada phase 2, Lagos")
+
+    # Test Distance between 2 points
+    print(addr1)
+    print(addr2)
+    loca = (addr1["data"]["lat"], addr1["data"]["lng"])
+    locb = (addr2["data"]["lat"], addr2["data"]["lng"])
+
+    inMiles = False
+    print(distanceBetweenLocations(loca, locb, inMiles), "km")
+
