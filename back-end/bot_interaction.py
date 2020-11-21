4fce1e93d354d2a5c6abae8918dc4ae33a9c37af
Mohamed Amine Belabbes <m.amine.belabbes@gmail.com>
2020-11-21 16:04:53 +0000

    Capture message

---
+++ back-end/bot_interaction.py
@@ -0,0 +1,8 @@
+""""
+    Defining the both answers to specific messages
+
+"""
+
+
+def processMessage(msg_received):
+    print(msg_received)
--- back-end/stream_events.py
+++ back-end/stream_events.py
@@ -1,11 +1,16 @@
 from twitivity import Event
-import json
+import json, bot_interaction
+
 
 class StreamEvent(Event):
-     CALLBACK_URL: str = "https://29f556047377.ngrok.io/payload"
+    CALLBACK_URL: str = "https://29f556047377.ngrok.io/payload"
 
-     def on_data(self, data: json) -> None:
-         print(data.keys())
+    def on_data(self, data: json) -> None:
+        event_type = list(data.keys())[1]
+        if "direct_message_events" == event_type:
+            if data["direct_message_events"][0]["message_create"]["sender_id"] != '1328476914600833025':
+                bot_interaction.processMessage(data["direct_message_events"][0]["message_create"]["message_data"]['text'])
+
 
 stream_events = StreamEvent()
 stream_events.listen()
