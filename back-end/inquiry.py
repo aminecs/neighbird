4170c35cc145ac1c5350df8908d37943c0d1c3df
nnobinelo <nnobinelo98@live.com>
2020-11-22 00:28:30 -0500

    added nlp model for comparing inquiries

--- back-end/inquiry.py
+++ back-end/inquiry.py
@@ -1,4 +1,5 @@
 from enum import IntEnum
+from nlp import NLPAnalyzer
 
 
 class InquiryType(IntEnum):
@@ -10,11 +11,17 @@ class InquiryType(IntEnum):
 class Inquiry:
     def __init__(self, user, inquiry_type, inquiry_str, time_limit=None):
         self.user = user  # (User) user class associated with this inquiry
-        self.location = user.location_info  # (dict) same as user.location_info
+        if user:
+            self.location = user.location_info  # (dict) same as user.location_info
+        else:
+            self.location = None
         self.inquiry_type = InquiryType(inquiry_type)  # (Enum) type of inquiry, see InquiryType
         self.inquiry_str = inquiry_str  # (str) the open-response text the user sent to chat bot describing their inquiry
         self.time_limit = time_limit  # (int) time limit in seconds that the inquiry will exist for
         self.matched_user = None  # (User) user class of other user that matches with this inquiry
+
+        nlp = NLPAnalyzer.get_instance()
+        self.entities = nlp.get_entity_analysis(inquiry_str)  # (dict) list of named entities from inquiry_str for NLP analysis
 
     def match_inquiry(self, user):
         self.matched_user = user
--- back-end/nlp.py
+++ back-end/nlp.py
@@ -1,25 +1,117 @@
 # Imports the Google Cloud client library
 from google.cloud import language_v1
+import spacy
+
+SIMILARITY_THRESHOLD = 0.7
 
 
-def extract_topic(inquiry_str):
-    pass
+class NLPAnalyzer:
+    __instance = None
+    __gcp_client = None
+    __spacy_nlp = None
+
+    @staticmethod
+    def get_instance():
+        if NLPAnalyzer.__instance is None:
+            NLPAnalyzer.__instance = NLPAnalyzer()
+            NLPAnalyzer.__gcp_client = language_v1.LanguageServiceClient()
+            NLPAnalyzer.__spacy_nlp = spacy.load("en_core_web_lg")
+        return NLPAnalyzer.__instance
+
+    def sort_by_cosine_similarity(self, target_inquiry, other_inquiries):
+        target_doc = self.__spacy_nlp(target_inquiry.inquiry_str)
+
+        sorted_inquiries = []
+        for inquiry in other_inquiries:
+            sorted_inquiries.append((inquiry, target_doc.similarity(self.__spacy_nlp(inquiry.inquiry_str))))
+
+        sorted_inquiries = sorted(sorted_inquiries, key=lambda inquiry_tuple: inquiry_tuple[1], reverse=True)
+        return sorted_inquiries
+
+    def sort_by_sentiment_similarity(self, target_inquiry, other_inquiries):
+        target_score = self._sentiment_score(target_inquiry.entities)
+
+        sorted_inquiries = []
+        for inquiry in other_inquiries:
+            sorted_inquiries.append((inquiry, abs(abs(target_score) - abs(self._sentiment_score(inquiry.entities)))))
+
+        sorted_inquiries = sorted(sorted_inquiries, key=lambda inquiry_tuple: inquiry_tuple[1])
+        return sorted_inquiries
+
+    def sort_by_cosine_and_sentiment_similarity(self, target_inquiry, other_inquiries):
+        target_score = self._sentiment_score(target_inquiry.entities)
+        target_doc = self.__spacy_nlp(target_inquiry.inquiry_str)
+
+        sorted_inquiries = []
+        for inquiry in other_inquiries:
+            similarity = target_doc.similarity(self.__spacy_nlp(inquiry.inquiry_str))
+            sentiment = self._sentiment_score(inquiry.entities)
+            sorted_inquiries.append((inquiry, abs(abs(1 * target_score) - abs(similarity * sentiment))))
+
+        sorted_inquiries = sorted(sorted_inquiries, key=lambda inquiry_tuple: inquiry_tuple[1])
+        return sorted_inquiries
+
+    def get_entity_analysis(self, text, print_info=False):
+        # The text to analyze
+        document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+        # retrieve entities and their sentiments
+        response = self.__gcp_client.analyze_entity_sentiment(document=document, encoding_type='UTF32')
+
+        if print_info:
+            print(f'--------------------------------------------------------------------\ntext: {text}')
+            for entity in response.entities:
+                print(f'entity: {entity.name}, sentiment score: {entity.sentiment.score}, '
+                      f'sentiment magnitude: {entity.sentiment.magnitude}, salience: {entity.salience}')
+
+        entities = [{'entity': entity.name,
+                     'sentiment_score': entity.sentiment.score,
+                     'sentiment_magnitude': entity.sentiment.magnitude,
+                     'salience': entity.salience} for entity in response.entities]
+
+        # document = language_v1.Document(content=text * 20, type_=language_v1.Document.Type.PLAIN_TEXT)
+        # # classify text
+        # response = client.classify_text(request={'document': document})
+        # categories = response.categories
+        # print(categories)
+
+        return entities
+
+    def _sentiment_score(self, entities):
+        score = 0
+        for entity in entities:
+            if entity['sentiment_score'] > 0:
+                sign = 1
+            else:
+                sign = -1
+            score += entity['sentiment_score'] * entity['sentiment_magnitude'] + entity['salience'] * sign
+        return score
 
 
 if __name__ == "__main__":
     """
-    Natural Language API sample code
+    testing out sentiment and cosine similarity
     """
+    from inquiry import Inquiry
 
-    # Instantiates a client
-    client = language_v1.LanguageServiceClient()
+    analyzer = NLPAnalyzer.get_instance()
 
-    # The text to analyze
-    text = "i like apples"
-    document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+    target_inquiry = 'basketball game last night'
+    other_inquiries = ['i loved the basketball game yesterday', 'i hated the basketball game yesterday',
+                       'the basketball game yesterday', 'i did not watch the basketball game yesterday',
+                       'i liked cooking last night']
 
-    # Detects the sentiment of the text
-    sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+    target_inquiry = Inquiry(None, 1, target_inquiry)
+    other_inquiries = [Inquiry(None, 1, oi) for oi in other_inquiries]
 
-    print(f'text:{text}')
-    print(f'sentiment: {sentiment.score}, {sentiment.magnitude}')
+    print(f'target inquiry: "{target_inquiry.inquiry_str}", '
+          f'sentiment score={analyzer._sentiment_score(target_inquiry.entities)}, entities: {target_inquiry.entities}')
+    print('---------------------------------- sorted by cosine similarity-----------------------------')
+    for i in analyzer.sort_by_cosine_similarity(target_inquiry, other_inquiries):
+        print(i[0].inquiry_str, i[1])
+    print('---------------------------------- sorted by sentiment similarity-----------------------------')
+    for i in analyzer.sort_by_sentiment_similarity(target_inquiry, other_inquiries):
+        print(i[0].inquiry_str, i[1], i[0].entities)
+    print('------------------------------- sorted by combination of cosine and sentiment ------------------------')
+    for i in analyzer.sort_by_cosine_and_sentiment_similarity(target_inquiry, other_inquiries):
+        print(i[0].inquiry_str, i[1])
